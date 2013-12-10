d5289701dc2dd408401a9966cdf8b990739c8420
omatsola sobotie <tsola2002@yahoo.co.uk>
2013-12-10 11:48:24 +0000

    last minute changes

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 </head>
 <body>
     <h1>Hello, world!</h1>
+    <?php echo "hello from php"; ?>
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="_/js/bootstrap.js"></script>
