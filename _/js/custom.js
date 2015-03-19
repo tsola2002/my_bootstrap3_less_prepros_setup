8b3e175f8ed5671803e8efda8571281270ac4b66
Omatsola Sobotie <tsola2002@yahoo.co.uk>
2015-03-19 17:11:07 +0000

    adding javascript scrollspy and scroll-smoothing to our navigation

--- _/js/custom.js
+++ _/js/custom.js
@@ -5,8 +5,38 @@
  * Time: 22:29
  * To change this template use File | Settings | File Templates.
  */
+$(function() {
 
-$('.carousel').carousel({
-    interval: false
+    "use strict";
+
+    var topoffset = 50; //variable for menu height
+
+    //Activate Scrollspy
+    $('body').scrollspy({
+        target: 'header .navbar',
+        offset: topoffset
+    });
+
+    var hash = $(this).find('li.active a').attr('href');
+    if(hash !== '#featured') {
+        $('header nav').addClass('inbody');
+    } else {
+        $('header nav').removeClass('inbody');
+    }
+
+    // Add an inbody class to nav when scrollspy event fires
+    $('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
+        var hash = $(this).find('li.active a').attr('href');
+        if(hash !== '#featured') {
+            $('header nav').addClass('inbody');
+        } else {
+            $('header nav').removeClass('inbody');
+        }
+    });
+
+    $('.carousel').carousel({
+        interval: false
+    });
+
 });
 
